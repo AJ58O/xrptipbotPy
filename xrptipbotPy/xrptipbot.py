7b9c91b3f56d5ffc09f127153a5cd3f4d1a677a0
AJ58O <breakthroughconsultations@gmail.com>
2019-09-15 01:04:40 -0700

    bug fix

--- xrptipbotPy/xrptipbot.py
+++ xrptipbotPy/xrptipbot.py
@@ -7,77 +7,86 @@ class xrptipbot:
 
 	def login(self, platform, model):
 		url = self.baseUrl + "/action:login/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token,
 			"platform":platform,
 			"model":model
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content
 
 	def unlink(self):
 		url = self.baseUrl + "/action:unlink/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content
 
 	def get_balance(self):
 		url = self.baseUrl + "/action:balance/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content
 
 	def tip(self, amount, to, existingDestination):
 		url = self.baseUrl + "/action:tip/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token,
 			"amount":amount,
 			"to":to,
 			"existingDestination":existingDestination
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content
 
 	def get_stats(self):
 		url = self.baseUrl + "/action:userinfo/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content
 
 	def get_contacts(self):
 		url = self.baseUrl + "/action:contacts/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content
 
 	def lookup_user(self, query):
 		url = self.baseUrl + "/action:lookup/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token,
 			"query":query
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content
 
 	def create_paper_wallet(self, note):
 		url = self.baseUrl + "/action:paper-proposal/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token,
 			"note":note
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content
 
 	def bump(self, amount, aps=None, geo=None, nfc=None):
 		url = self.baseUrl + "/action:bump/"
+		headers = {"Content-Type":"application/json"}
 		payload = {
 			"token":self.token,
 			"amount":amount,
@@ -85,6 +94,6 @@ class xrptipbot:
 			"geo":geo,
 			"nfc":nfc
 		}
-		r = requests.post(url=url, data=payload)
+		r = requests.post(url=url, json=payload, headers=headers)
 		return r.content		
 
